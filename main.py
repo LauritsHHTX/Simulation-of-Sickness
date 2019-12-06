5023bc344df1589ddeae42d7f9a5c13408a27540
laurits <Laurits.vest.hansen@gmail.com>
2019-12-06 09:34:08 +0100

    Song may work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arcade
 import song
+import key
 
 windowWidth = 900
 windowHeight = 600
@@ -11,13 +12,19 @@ class Window(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title) # Call the parent class's init function
         self.song = song.Song
-
+        self.listOfKeys = []
+        for i in range(1, 4):
+            self.listOfKeys.append(key.Key(i))
 
     def update(self, delta_time):
-        pass
+        for i in self.song.listOfNotes:
+            i.Move()
 
     def on_draw(self):
-        pass
+        for i in self.song.listOfNotes:
+            i.Draw()
+        for i in self.listOfKeys:
+            i.Draw()
 
     def OnKeyPress(self):
         pass
