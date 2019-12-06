715678c217f54c976e831b3b9a75d5e32b0151d1
DevilishMutant <andreas.gkp@gmail.com>
2019-12-06 09:53:48 +0100

    fixed some shit

--- key.py
+++ key.py
@@ -19,15 +19,14 @@ class Key:
         if self.type == 4:
             self.color = arcade.color.OLD_GOLD
 
-
     def Draw(self):
         arcade.draw_circle_outline(self.xPos, self.yPos, self.radius, self.color, 0.5)
 
     def DetectCollision(self):
-        for x in main.theObjectToRuleThemAll.song.listOfNotes:
-            self.afstand = math.sqrt(()**2 + ()**2)
-            if self.afstand < x.r:
-                x.Destroy()
+        for obj in main.theObjectToRuleThemAll.song.listOfNotes:
+            self.afstand = math.sqrt(((self.xPos + self.radius) - (obj.x + obj.r))**2 + ((self.yPos + self.radius) - (obj.y + obj.r))**2)
+            if self.afstand < obj.r:
+                obj.Destroy()
 
             else:
                 pass
